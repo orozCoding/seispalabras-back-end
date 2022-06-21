d7ff2f6436964dfcc0baa5c1a71c1466950c297f
orozcoding <orozcoding@gmail.com>
2022-06-21 07:35:03 -0400

    Update migrations

--- db/migrate/20220621113235_create_users.rb
+++ db/migrate/20220621113235_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :role
-      t.string :email
+      t.string :name, null: false
+      t.string :role, default: "student"
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps
--- db/migrate/20220621113255_create_translations.rb
+++ db/migrate/20220621113255_create_translations.rb
@@ -1,8 +1,8 @@
 class CreateTranslations < ActiveRecord::Migration[7.0]
   def change
     create_table :translations do |t|
-      t.string :word
-      t.integer :word_id
+      t.string :word, null: false
+      t.integer :word_id, null: false
 
       t.timestamps
     end
