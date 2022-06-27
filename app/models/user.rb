813d0f31122fc3f89db4062f035de0d2507f4be2
orozcoding <orozcoding@gmail.com>
2022-06-27 17:24:24 -0400

    Improve validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,15 @@ class User < ApplicationRecord
   has_many :translations
   has_many :word_lists
 
-  validates :name, presence: true
-  validates :username, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, format: { without: /\s/, message: "Can't have blank spaces" },
+  length: { minimum: 3, maximum: 12 }
+  validates :username, presence: true, uniqueness: true,
+  format: { with: /\A[a-zA-Z0-9]+\Z/, message: "No special characters or spaces allowed" },
+  length: { minimum: 4, maximum: 12 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Use a valid email" }
   validates :password, presence: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
-  validates :username, uniqueness: true
-  validates :email, uniqueness: true
-
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 end
